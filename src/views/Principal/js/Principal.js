576ac649be034b993c1b1825e66a7397f68da926
Gatolino <andy.gatolino@gmail.com>
2017-08-17 13:28:39 -0300

    Adicionadas primeiras Views
    Adicionados primeiros modelos de Views

---
+++ src/views/Principal/js/Principal.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    $("#mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    $("#mySidenav").style.width = "0";
+}
