aff3ac98e29aba717e9e5a64561d1cd7572d9b68
Erik Vinicius <erik@jusbrasil.com.br>
2017-08-21 23:10:36 -0300

    Add items to stock

--- src/schema/stock/controller.js
+++ src/schema/stock/controller.js
@@ -25,7 +25,12 @@ export default class StockController extends BaseController {
 
   @route('get', '/estoques/:id')
   async read(req, res) {
-    return res.render('stocks/read.html', await this._dao.read(req.params.id));
+    const stock = await this._dao.read(req.params.id);
+    stock.items
+      .filter(i => i.kind === 'Weapon')
+      .forEach(({ item }) => { item.description = `${item.manufacturer} - ${item.model}` });
+
+    return res.render('stocks/read.html', stock);
   }
 
   @route('get', '/estoques/:id/items/novo')
@@ -34,7 +39,7 @@ export default class StockController extends BaseController {
       {
         key: 'Weapon',
         label: 'Arma',
-        items: (await WeaponDAO.all()).map(w => ({ ...w, description: w.model })),
+        items: (await WeaponDAO.all()).map(w => ({ ...w._doc, description: w.model })),
       },
       { key: 'Ammo', label: 'Munição', items: await AmmoDAO.all() },
       { key: 'Accessory', label: 'Acessório', items: await AccessoryDAO.all() },
@@ -47,6 +52,37 @@ export default class StockController extends BaseController {
     const [kind, id] = req.body.item.split('|');
     const { quantity } = req.body;
     await this._dao.addItem(req.params.id, { kind, id, quantity });
-    return res.redirect(`/estoques/${req.params.id}/items`);
+    return res.redirect(`/estoques/${req.params.id}`);
+  }
+
+  @route('get', '/estoques/:id/items/:itemId/editar')
+  async editItem(req, res) {
+    const kinds = [
+      {
+        key: 'Weapon',
+        label: 'Arma',
+        items: (await WeaponDAO.all()).map(w => ({ ...w._doc, description: w.model })),
+      },
+      { key: 'Ammo', label: 'Munição', items: await AmmoDAO.all() },
+      { key: 'Accessory', label: 'Acessório', items: await AccessoryDAO.all() },
+    ];
+    const { id, itemId } = req.params;
+    const stock = await this._dao.read(id);
+    const item = stock.items.find(i => i.id === itemId);
+    return res.render('stocks/editItem.html', { stockId: id, kinds, ...item._doc });
+  }
+
+  @route('patch', '/estoques/:id/items/:itemId')
+  async updateItem(req, res) {
+    const stock = await this._dao.findOne({ id: req.params.id });
+
+    const { item, quantity } = req.body;
+    const [kind, id] = item.split('|');
+
+    stock.items
+      .filter(i => i.id === req.params.itemId)
+      .forEach(i => Object.assign(i, { item: id, kind, quantity }));
+
+    return res.send(await stock.save());
   }
 }
